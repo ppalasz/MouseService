57c823a83d73d126d21a22ce95260a0ae797aa2c
ppalasz <40823206+ppalasz@users.noreply.github.com>
2023-12-06 09:24:53 +0100

    Add files via upload

---
+++ MouseService/MouseMoveService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace MouseService
+{
+    public partial class MouseMoveService : ServiceBase
+    {
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+        private const int MOUSE_TIME = 500;
+        private Timer timer;
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, int dwExtraInfo);
+
+
+        public MouseMoveService()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer();
+            timer.Interval = MOUSE_TIME; // Ustaw interwał na 1 minutę
+            timer.Elapsed += new ElapsedEventHandler(OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // Symulacja ruchu myszy
+            MoveMouse();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+
+        private void MoveMouse()
+        {
+            mouse_event(MOUSEEVENTF_MOVE, 0, 1, 0, 0);
+            Thread.Sleep(MOUSE_TIME); 
+            mouse_event(MOUSEEVENTF_MOVE, 0, -1, 0, 0);
+        }
+    }
+}
---
+++ MouseService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            // Uruchom jako zwykła aplikacja konsolowa
+            MouseMoveService myService = new MouseMoveService();
+            myService.OnDebug();
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#else
+            // Uruchom jako usługa
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MouseMoveService()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+
+
+        }
+    }
+}
